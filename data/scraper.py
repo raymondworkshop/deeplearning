52e63a79ff267951a6e6fc6c5f8906a70333fe52
Raymond Zhao <raymondzhao@hsmc.edu.hk>
2018-06-25 12:34:17 +0800

    update

--- data/scraper.py
+++ data/scraper.py
@@ -5,27 +5,24 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import re
-# from lxml import html
-
+#from lxml import html  
+#import pickle
 
 AMAZON_URL = "https://www.amazon.com/s/?keywords=laptop"
 AMAZON_DP = "https://www.amazon.com/dp/"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
 
 def is_result_div(tag):
     """ Returns true for divs with ids starting with 'result_'. """
     if not tag.name == "div":
         return False
-    # id="result_17"
+    #id="result_17"
     return tag.has_key("id") and str(tag["id"]).startswith("result_")
 
-
 def get_asins(html):
-    # keep the values in list
+    #keep the values in list
     # data-asin list
-    # results = []
+    #results = []
 
     #
     soup = BeautifulSoup(html, 'html5lib')
@@ -34,52 +31,60 @@ def get_asins(html):
     for result_div in is_result_divs:
         asin = result_div.find('li')
     """
-    asins = soup.findAll("div", attrs={"data-asin": re.compile(r".*")})
+    asins = soup.findAll("div", attrs = {"data-asin": re.compile(r".*")})
     if len(asins) == 0:
-        asins = soup.findAll("li", attrs={"data-asin": re.compile(r".*")})
+        asins = soup.findAll("li", attrs = {"data-asin": re.compile(r".*")})
 
     lst_asins = []
     i = 0
     while i < len(list(asins)):
         print(asins[i])
-        # asin
-
+        #asin
+        
         lst_asins.append(asins[i]["data-asin"])
         i = i + 1
 
     return lst_asins
 
+def num_there(s):
+    return re.search(r'[2]\d{3}', s)
 
 def get_results(asin):
-        # Added Retrying
-                        # This script has only been tested with Amazon.com
+    # Added Retrying 
     _dict = {}
-    url = 'http://www.amazon.com/dp/' + asin
-    # Add some recent user agent to prevent amazon from blocking the request
-    # Find some chrome user agent strings  here https://udger.com/resources/ua-list/browser-detail?browser=Chrome
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-    r = requests.get(url, headers=headers)
-    # page_response = r.content
+    url  = 'http://www.amazon.com/dp/'+asin
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+    r = requests.get(url,headers = headers )
+    #page_response = r.content
     soup = BeautifulSoup(r.content, 'html5lib')
     # reviews, technical specifications, price, brand
-    # title
-    # price - id "style_name_0_price"
-    # product_title = soup.title.string
-    # product_price = soup.find(id="style_name_0_price").text
+        #title
+        # price - id "style_name_0_price"
+    #product_title = soup.title.string
+    # #product_price = soup.find(id="style_name_0_price").text
+    _dict["asin"] = asin
     _dict["title"] = soup.title.string
-    _product_price = soup.find(id="style_name_0_price")
 
-    import pdb
-    pdb.set_trace()
-    if _product_price:
-        _dict["price"] = _product_price.text
+    product_price = soup.find(id="style_name_0_price")
+    if product_price:
+        _dict["price"] = product_price.text.rstrip('\n')
+    else:
+        _dict["price"] = ""
 
-    # product_date = soup.find(id="averageCustomerReviews").text
+	#product_date = soup.find(id="averageCustomerReviews").text
+    #
+    #_add_info =  soup.find_all('tr.th', {'class': "a-color-secondary a-size-base prodDetSectionEntry"})
+    _add_info = soup.find(id="productDetails_detailBullets_sections1")
+    if _add_info:
+        rows = _add_info.findChildren(['td'])
+        for row in rows:
+            _td = str(row.get_text().rstrip("\n").lstrip("\n"))
+            if num_there(_td):
+                #_td = _tr.td.get_text().lstrip().rstrip().rstrip("\n").lstrip("\n")
+                _dict["date"] = _td
 
     _tech_details = {}
-    _tech_details_1 = soup.find(
-        id="productDetails_techSpec_section_1")
+    _tech_details_1 = soup.find(id="productDetails_techSpec_section_1")
     if _tech_details_1:
         tech_details_1 = _tech_details_1.find_all("tr")
         for _tr in tech_details_1:
@@ -87,8 +92,7 @@ def get_results(asin):
             _td = _tr.td.get_text()
             _tech_details[_th] = _td
 
-    _tech_details_2 = soup.find(
-        id="productDetails_techSpec_section_2")
+    _tech_details_2 = soup.find(id="productDetails_techSpec_section_2")
     if _tech_details_2:
         tech_details_2 = _tech_details_2.find_all("tr")
         for _tr in tech_details_2:
@@ -96,33 +100,36 @@ def get_results(asin):
             _td = _tr.td.get_text()
             _tech_details[_th] = _td
 
-    _dict["tech"] = _tech_details
+    if _tech_details:
+        _dict["tech"] = _tech_details
+    else:
+        _dict["tech"] = ""
 
-    _reviews = soup.find_all(
-        'div', {'class': "a-expander-content a-expander-partial-collapse-content"})
-    """
-	reviews = []
-    for item in _reviews:
-	    reviews.append(item.get_text().encode('utf-8'))
-	"""
+    #_reviews = soup.find(id="customerReviews")
+    _reviews_txt =  soup.find_all('div', {'class' : "a-expander-content a-expander-partial-collapse-content"})
+    reviews = []
+    for item in _reviews_txt:
+        txt = item.get_text().lstrip().rstrip().rstrip("\n").lstrip("\n")
+        txt_list = txt.split()
+        if len(txt_list) > 5: # bad option
+            reviews.append(txt.encode('utf-8'))
 
-    _dict["reviews"] = str(_reviews)
-    # if not reviews:
+    _dict["reviews"] = reviews
+    #if not reviews:
     #	reviews = parser.xpath(XPATH_REVIEW_SECTION_2)
 
     return _dict
 
-
 def main():
-    """
+    
     # get all asins of some product
     ASINS =  []
 
     # scrape all the pages
-    # total = 0
+    #total = 0
     page = 1 # by default
-    pages = 20
-    # while True:
+    pages = 400
+    #while True:
     while page < pages:
         url = AMAZON_URL + "&page=" + str(page)
         print("Scraping page %d url: %s " % (page, url))
@@ -130,31 +137,43 @@ def main():
 
         r = requests.get(url,headers = headers)
         results = get_asins(r.content)
-
-        # store
+        
+        #store
         ASINS = ASINS + results
         print(ASINS)
-    """
+    
+    #write the file
+    with open("amazon_asin_update.md", 'a') as f:
+        f.write(str(ASINS))
+        
 
-    # get the infor based on ASINS
+    #get the infor based on ASINS
     # reviews, technical specifications, price, brand
-    # title
+    #title
     # price - id "style_name_0_price"
-    ASINS = ["B07C7XY7GS", "B07C8J1NT"]
-    f = open('amazon.json', 'a')
-    # f = open('amazon.json', 'a')
+    
+    #ASINS = ["B07C7XY7GS", "B017XR0XWC"]
+    #f = open()
+    """
+    f1 = open("amazon_asin.md", 'r')
+    ASINS = eval(f1.readline())
+
+    #ASINS = ["B07CYX3DG8"]
+    #ASINS = ["B01MYGF32C"]
+  
+    f = open('amazon_update.json', 'a', encoding="utf-8") 
     _ret = {}
     for asin in ASINS:
         _ret = get_results(asin)
         print(_ret)
-        f.write(json.dumps(_ret))
+        f.write(str(_ret) + "\n")
 
     f.close()
+    """
 
-    # print(_ret)
-    # print(get_results)
+    #print(_ret)
+    #print(get_results)
     return 0
-
 
 if __name__ == "__main__":
     main()
