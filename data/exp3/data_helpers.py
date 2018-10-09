a62fe498aa2b64fc6bc6775f645d32e16e048e95
wenlzhao <wenlzhao@gmail.com>
2018-10-09 14:11:53 +0800

    add exp3

---
+++ data/exp3/data_helpers.py
@@ -0,0 +1,421 @@
+import numpy as np
+import re
+
+import ast
+from keras.utils import to_categorical
+
+
+
+def get_cpu_label(_str):
+    # [ 2 GHz AMD A Series, 1.1 GHz Intel Celeron, 2.16 GHz Intel Celeron,3 GHz 8032,1.6 GHz,3.5 GHz 8032,4 GHz Intel Core i7]
+    _cpu_map = {
+        "amd": 0,
+        "1.1 Intel":1,
+        "1.5-2.5 Intel": 2,
+        "2.5-3.5 Intel":3,
+        "3.5 Intel":4,
+        "others":5
+         
+    }
+
+    #_cpu_label = 4 #unknown
+    if 'amd' in _str.lower():
+        _cpu_label = 0
+    else: #Intel
+        _cpu_frequency = float(re.search('[\d]+[.\d]*', _str).group())   
+        if _cpu_frequency <= 1.5:
+            _cpu_label = 1
+        elif _cpu_frequency <= 2.5:
+            _cpu_label = 2
+        elif _cpu_frequency <= 3:
+            _cpu_label = 3
+        elif _cpu_frequency <= 3.5:
+            _cpu_label = 4
+        elif _cpu_frequency > 3.5:
+            _cpu_label = 5
+        
+        #_cpu_label = 1
+
+    return _cpu_label
+
+def get_sscreen_label(_str):
+    # [ 11.6 inches, 13.3 inches,14 inches,15.6 inches, 17.3 inches ]
+    _sscreen_map = {
+        "<= 12 inches": 0,
+        "<= 13 inches":1,
+        "<= 14 inches": 2,
+        "<= 15 inches":3,
+        "> 15 inches":4
+    }
+
+    _sscreen_label = 4 #unknown
+    if 'inches' in _str.lower():
+        _sscreen_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+        if _sscreen_size <= 12:
+            _sscreen_label = 0
+        elif _sscreen_size <= 13:
+            _sscreen_label = 1
+        elif _sscreen_size  <= 14:
+            _sscreen_label = 2
+        elif _sscreen_size <= 15:
+            _sscreen_label = 3
+        else:
+            _sscreen_label = 4
+
+    return _sscreen_label
+
+
+def get_ram_label(_str):
+    # [ "4 GB SDRAM DDR3", "4 GB DDR3 SDRAM","8 GB",4 GB SDRAM DDR4","16 GB DDR4" ,"2 GB SDRAM","6 GB DDR SDRAM", "12 GB DDR SDRAM" ]
+    _ram_map = {
+        "2 GB SDRAM": 0,
+        "4 GB SDRAM DDR3": 1,
+        "6 GB DDR SDRAM":2,
+        "8 GB SDRAM DDR3": 3,
+        "8 GB SDRAM DDR4": 3,
+        "12 GB DDR SDRAM":4,
+        "16 GB DDR4" :5,
+        "others":6,
+    }
+
+    #_ram_label = 7 #unknown
+    if 'gb'  in _str.lower():
+        _ram_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+        if _ram_size == 2:
+            _ram_label = 0
+        elif _ram_size == 4:
+            _ram_label = 1
+        elif _ram_size  == 6:
+            _ram_label = 2
+        elif _ram_size == 8:
+            if 'ddr3' in _str.lower():
+                _ram_label = 3
+            elif 'ddr4' in _str.lower():
+                _ram_label = 3
+            else:
+                _ram_label = 3
+        elif _ram_size  == 12:
+            _ram_label = 4
+        elif _ram_size  == 16:
+            _ram_label = 5
+        else:
+            #_ram_label = 7
+            pass
+
+    return _ram_label
+
+def get_harddrive_label(_str):
+    # [ '16 GB SSD', '128 GB SSD', '1 TB HDD 5400 rpm', 
+    # '256 GB Flash Memory Solid State', '500 GB HDD 5400 rpm', 
+    # 'Flash Memory Solid State', '1000 GB Hybrid Drive',
+    # '2 TB HDD 5400 rpm', '32 GB SSD','64 GB SSD'
+    #
+    # ]
+    _harddrive_map = {
+        "SSD <= 128": 0,
+        "SSD > 128": 1,
+        "HDD > 1T" :2,
+        "HDD ~= 1T" :3,
+        "HDD ~= 500G" :4,
+        "HDD < 500G" :5,
+        "others": 5
+    }
+
+    #_harddrive_label = 5 #unknown
+    if 'ssd' or 'solid' or 'mechanical' in _str.lower():
+        if num_there(_str):
+            _harddrive_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+            if _harddrive_size <= 128:
+                _harddrive_label = 0
+            else:
+                _harddrive_label = 0
+        else:
+            _harddrive_label = 0
+
+    if 'hdd' in _str.lower():
+        #_harddrive_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+        if 'tb' in _str.lower():
+            if num_there(_str):
+                _harddrive_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+                if _harddrive_size > 1:
+                    _harddrive_label = 1
+                else:
+                    _harddrive_label = 2
+        else:
+            if num_there(_str):
+                _harddrive_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+                if _harddrive_size >= 500:
+                    _harddrive_label = 3
+                else:
+                    _harddrive_label = 4
+
+    return _harddrive_label
+
+
+def num_there(s):
+    return any(i.isdigit() for i in s)
+
+def get_graphprocessor_label(_str):
+    """
+    [ 'Intel HD Graphics 500', 'Intel HD Graphics 505', 'Intel UHD Graphics 620',
+    'AMD', 'NVIDIA GeForce GTX 1050', 'GTX 1050 Ti'
+      'PC', 'FirePro W4190M - AMD', 'Integrated', 
+
+    ]
+    """
+    _graphprocessor_map = {
+        "Intel HD Graphics 50X": 0,
+        "Intel HD Graphics 505": 0,
+        "Intel UHD Graphics 620":1,
+        "Intel HD Graphics" :1,
+        "AMD Radeon R2": 3,
+        "AMD Radeon R5": 4,
+        "AMD Radeon R7": 4,
+        "AMD Radeon R4" :3,
+        "NVIDIA GeForce GTX 1050": 58,
+        "NVIDIA GeForce 940MX" :  5,
+        "Integrated" : 6,
+        "others| PC | FirePro W4190M ": 7
+    }
+
+    _graphprocessor_label = 5 #unknown
+    if 'intel' in _str.lower():
+        
+        if num_there(_str):
+            _graphprocessor_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+            if _graphprocessor_size == 500:
+                _graphprocessor_label = 0
+            elif _graphprocessor_size == 505:
+                _graphprocessor_label = 0
+            elif _graphprocessor_size  == 620:
+                _graphprocessor_label = 0
+            else:
+                _graphprocessor_label = 0
+
+    if 'amd' in _str.lower():        
+        if 'r2' in _str.lower():
+            _graphprocessor_label = 1
+        if 'r5' in _str.lower():
+            _graphprocessor_label = 2
+        if 'r7' in _str.lower():
+            _graphprocessor_label =2
+        if 'r4' in _str.lower():
+            _graphprocessor_label = 1
+        
+
+    if 'nvidia' in _str.lower():
+        if num_there(_str):
+            _graphprocessor_size = int(float(re.search('[\d]+[.\d]*', _str).group()))
+            if _graphprocessor_size == 1050:
+                _graphprocessor_label = 3
+            if _graphprocessor_size == 940:
+                _graphprocessor_label = 3
+        
+
+    if 'integrated' in _str.lower():
+        _graphprocessor_label  = 4
+
+
+    return _graphprocessor_label
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+def load_data_and_labels(positive_data_file, negative_data_file):
+    """
+    Loads MR polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    positive_examples = list(open(positive_data_file, "r", encoding='utf-8').readlines())
+    positive_examples = [s.strip() for s in positive_examples]
+    negative_examples = list(open(negative_data_file, "r", encoding='utf-8').readlines())
+    negative_examples = [s.strip() for s in negative_examples]
+    # Split by words
+    x_text = positive_examples + negative_examples
+    x_text = [clean_str(sent) for sent in x_text]
+    # Generate labels
+    positive_labels = [[0, 1] for _ in positive_examples]
+    negative_labels = [[1, 0] for _ in negative_examples]
+    y = np.concatenate([positive_labels, negative_labels], 0)
+    return [x_text, y]
+
+def read_data(file):
+    list_reviews = []
+    asins = {}
+
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            data = ast.literal_eval(str(line))
+            """
+            if 'reviews' in line:
+                #line = line.replace("\'", "\"")
+                reviews = data['reviews']      
+               # len_review = 0
+                #cnt = 0
+                #mean_review = 0
+                #max_len_review = 0
+                for review in reviews:
+                    list_reviews.append(review)
+            """
+
+            if 'tech' in line and len(data['tech']) > 0 and 'reviews' in line:
+                _asin = str(data['asin'])
+
+                reviews = data['reviews']
+                
+                for review in reviews:
+                    list_reviews.append(review)
+                
+
+                params = data['tech']
+                if len(params) > 0:
+                    #asins[_asin] = [screensize,cpu, ram]
+                    asins[_asin] = []
+                    
+                    for key, value in params.items():
+                        if 'processor' in key.lower():
+                            asins[_asin].append(value)
+                        if 'ram' in key.lower():
+                            asins[_asin].append(value)    
+                        if 'screen size' in key.lower():
+                            asins[_asin].append(value)
+                        #hard drive
+                        if 'hard' in key.lower():
+                            asins[_asin].append(value)
+                        # Graphics Coprocessor
+                        if 'Graphics Coprocessor' in key.lower():
+                            asins[_asin].append(value)
+
+                        if len(asins[_asin]) == 5:
+                            break
+
+                    #asins[_asin].append(list_reviews)
+                    asins[_asin].append(reviews)
+
+    return asins
+
+def load_data_and_labels_update(positive_data_file, negative_data_file):
+    """
+    Loads MR polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    #dir = "/data/raymond/workspace/exp2/"
+
+    #dir = 'C:/Users/raymondzhao/myproject/dev.deeplearning/data/'
+    dir = '/data/raymond/workspace/exp3/data/'
+    #file = dir + 'amazon_reviews.json'
+    file = dir + 'amazon_reviews_copy.json'
+    asins = read_data(file)
+
+    file = dir + 'amazon_tech_all_5.csv'
+    #df = pd.read_csv(file)
+
+    # The text samples and their labels
+    texts = []  #list of text samples
+    #labels = array([])
+    labels = [] #list of label ids
+    labels_index = {}  # dictionary mapping label name to numeric id
+
+    # ['14 inches', '2.16 GHz Intel Celeron', '4 GB SDRAM DDR3', 
+    # [[b'I', b'placed', b'my', b'order', b'on', b'December', b'19th', b'and', b'was', b'under', b'the', b'impression', b'it', b'would', b'arrive', b'on', b'the', b'22nd']]
+        # [screensize,cpu, ram, Hard Drive,Graphics Coprocessor, reviews]
+    for _asin in asins:
+        print("The asin %s:", _asin)
+        # [screensize,cpu, ram, reviews]
+        _cpu = asins[_asin][1]
+        if _cpu:
+           _cpu_id = get_cpu_label(_cpu)
+           labels_index[_cpu] = _cpu_id
+
+        _sscreen = asins[_asin][0]
+        if _sscreen:
+            _sscreen_id = get_sscreen_label(_sscreen)
+            labels_index[_sscreen] = _sscreen_id
+
+        _ram = asins[_asin][2]
+        if _ram:
+            _ram_id = get_ram_label(_ram)
+            labels_index[_ram] = _ram_id
+        
+        _harddrive = asins[_asin][3]
+        if _harddrive:
+            _harddrive_id = get_harddrive_label(_harddrive)
+            labels_index[_harddrive] = _harddrive_id
+    
+        #Graphics Coprocessor
+        _graphprocessor = asins[_asin][4]
+        if _graphprocessor:
+            _graphprocessor_id = get_graphprocessor_label(_graphprocessor)
+            labels_index[_graphprocessor] = _graphprocessor_id
+        
+        #reviews
+        reviews = asins[_asin][5] 
+        #table = str.maketrans('', '', string.punctuation)
+        #porter = PorterStemmer()
+        for _t in reviews:
+            # t =  " ".join(x.decode("utf-8") for x in _t) #bytes to str
+            #words = text.split()
+            # remove punctuation from each word , and stemming
+
+            #stripped = [w.decode("utf-8").lower().translate(table) for w in _t]  
+            #s = " ".join(x for x in stripped)
+            stripped = [w.decode("utf-8") for w in _t]
+            _s = " ".join(x for x in stripped)
+            s = clean_str(_s)
+            #stripped = [w.decode("utf-8").translate(table) for w in _t] 
+            #stripped = [w.decode("utf-8").lower().translate(table) for w in _t]
+            #
+
+            #t = re.sub('[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]+','', t)
+            texts.append(s)
+            #labels.append(_cpu_id)
+            #labels.append(_sscreen_id)
+            #labels.append(_ram_id)
+            labels.append(_harddrive_id)
+            #labels.append(_graphprocessor_id)
+
+    labels = to_categorical(np.asarray(labels))
+
+    return [texts, labels]
+
+
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
