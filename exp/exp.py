9993625b4527f4a58c9be23b5d6d58fe27cb531a
wenlzhao <wenlzhao@gmail.com>
2018-12-27 18:06:36 +0800

    update

--- exp/exp.py
+++ exp/exp.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 """
- This is an implementation
-"""
+ This is an exp
+ @raymond
 
+"""
+import numpy as np
 
 import os
 
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    #initialize
+
+    #train
+
+    #test
+    main()
